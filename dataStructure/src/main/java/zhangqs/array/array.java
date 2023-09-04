6e0ebe03d0a284b34357daa367ca990488669183
zhangqiushi <zhangqiushi@yunzhangfang.com>
2023-09-05 07:29:21 +0800

    array 成员变量和初始化

--- dataStructure/src/main/java/zhangqs/array/array.java
+++ dataStructure/src/main/java/zhangqs/array/array.java
@@ -1,5 +1,37 @@
 package zhangqs.array;
 
+/**
+ *  实现一个 数组
+ *  新增 删除 查找
+ */
 public class array {
+
+    // 成员变量 不需要final 修饰 不需要在定义的时候初始化
+    // 通过构造函数 对 数组 以及 大小进行初始化
+    // 需要 默认值的时候 需要 在类初始化的时候 通过final 和 static 进行初始化
+
+    // 定一个空数组
+    public Object[] objects;
+
+    // 数组个数
+    public int size;
+
+    //默认的空数组
+    public static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+
+    public array() {
+        this.objects = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+    }
+
+    public array(int size) {
+        // 初始化数组
+        if (size > 0){
+            this.objects = new Object[size];
+        } else if (size == 0) {
+           this.objects = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal size: "+ size);
+        }
+    }
 
 }
---
+++ dataStructure/src/main/java/zhangqs/array/arrayList.java
@@ -0,0 +1,9 @@
+package zhangqs.array;
+
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<Object> arrayList = new ArrayList<>();
+    }
+}
