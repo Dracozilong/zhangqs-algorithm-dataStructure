f2c4284698d42c2944b01dee920783e8fe104b35
zhangqiushi <zhangqiushi@yunzhangfang.com>
2023-10-26 08:03:08 +0800

    singleLinkedList 在指定位置插入元素

--- dataStructure/src/main/java/zhangqs/linkedlist/SingleLinkedList.java
+++ dataStructure/src/main/java/zhangqs/linkedlist/SingleLinkedList.java
@@ -46,6 +46,10 @@ public class SingleLinkedList {
         size++;
     }
 
+    public int getSize(){
+        return size;
+    }
+
     /**
      * 在指定位置插入元素
      * @param index
@@ -64,9 +68,13 @@ public class SingleLinkedList {
         }
         // 3.1 找到当前需要插入的节点数据的原节点的数据 ，所以需要遍历链表
         Node curr = head;
-        for (int i = 0; i < index; i++) {
-
+        for (int i = 0; i < index-1; i++) {
+             curr =curr.next;
         }
+        Node node = new Node(value);
+        node.next =curr.next;
+
+
     }
 
     @Override
