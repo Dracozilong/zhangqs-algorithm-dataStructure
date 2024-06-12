ba7f50bd2168f31e4580d409c3842164e4c3487d
zhangqiushi <Dracozilong@126.com>
2024-06-13 07:14:41 +0800

    数组和栈 新增/删除返回值修改

--- dataStructure/src/main/java/zhangqs/stack/test/StackTest.java
+++ dataStructure/src/main/java/zhangqs/stack/test/StackTest.java
@@ -13,7 +13,7 @@ public class StackTest {
     private final Logger logger = LoggerFactory.getLogger(StackTest.class);
 
     public static void main(String[] args) {
-        Stack stack = new Stack(10);
+        Stack stack = new Stack(5);
         stack.push(1);
         stack.push(2);
         stack.push(3);
