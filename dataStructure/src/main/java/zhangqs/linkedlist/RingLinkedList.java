aaec6e79e31ca21ac112ccbc6e5aa833be837aaf
zhangqiushi <Dracozilong@126.com>
2025-12-07 11:42:22 +0800

    环形链表 新增

--- dataStructure/src/main/java/zhangqs/linkedlist/RingLinkedList.java
+++ dataStructure/src/main/java/zhangqs/linkedlist/RingLinkedList.java
@@ -12,8 +12,35 @@ public class RingLinkedList {
         this.head = new Node(null);
     }
 
-    // 循环链表 头插入法
+    // 循环链表 头尾区别不大  不用区分 头插 还是尾插
     // 先找到尾节点 → 让尾节点的 next 指向新节点 → 再让新节点 next 指向原 head → 最后更新 head 指向新节点
+
+    public void add(Integer data) {
+        if (data == null) {
+            return;
+        }
+        // 创建一个新节点
+        Node newNode = new Node(data);
+
+        // 当前 没有节点
+        if (head.next ==null){
+            // 当前创建的节点 作为第一个节点
+            head.next = newNode;
+            // 指向 自己
+            newNode.next = newNode;
+        }else {
+            // 找到尾节点sss
+            Node curr = head.next;
+            while (curr.next != head.next) {
+                curr = curr.next;
+            }
+            // 赋值 尾节点的 next
+            curr.next = newNode;
+            newNode.next = head.next;
+        }
+    }
+
+
 
 
     // 定义一个Node节点
